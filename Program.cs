d43013f0ab302e0d02a25cf9ef38f5123ceac8ae
Zepsen Dmytro <zepsendm1@gmail.com>
2017-04-24 13:14:36 +0300

    Complet with Happy G

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Codewars
     {
         static void Main(string[] args)
         {
-            ICodeWars war = new Wave();
+            ICodeWars war = new HappyG();
             var res = war.Start();
             Console.WriteLine(res.ToString());
         }
