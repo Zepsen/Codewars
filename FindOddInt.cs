590a7f48e775f16437840ccc5673ee98017726ad
Zepsen <zepsendm@gmail.com>
2017-05-07 19:35:13 +0300

    finish another one

--- FindOddInt.cs
+++ FindOddInt.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 
 public class FindOddInt : ICodeWars
 {
-    object ICodeWars.Start()
+    public object Start()
     {
         return Func(new [] {1,1,2,2,3,3,5});
     }
 
     public static int Func(int[] seq)
     {       
-        var res = seq.ToDictionary(i => i, i => 0);
+        var res = seq.GroupBy(i => i).Select(i => new { Key = i.Key, Value = i.Count()}).Single(i => i.Value % 2 != 0);    
+                
         return 0;
     }
 }
