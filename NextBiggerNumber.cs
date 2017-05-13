ec1629f705a41b5911b2115383e6aff9f5e38b7a
Zepsen <zepsendm@gmail.com>
2017-05-14 00:45:45 +0300

    clean up last

--- NextBiggerNumber.cs
+++ NextBiggerNumber.cs
@@ -11,38 +11,32 @@ namespace Codewars
     {
         public object Start()
         {
-            return Func(201211);
+            return Func(13111);
         }
 
         public int Func(int num)
         {
             var list = num.ToString().Select(n => n - '0').ToList();
             var indx = 0;
-            var res = new List<int>();
-
-            for(int i = list.Count - 1 ; i > 0; i--)
-            {
-                if(indx == 0)
-                {
+           
+            for(int i = list.Count - 1 ; i > -1; i--)
+            {                
                     if(i != 0 && list[i] > list[i-1])
                     {
-                        indx = i-1;                                       
-                    }
-                }                
-                else 
-                {
-                    res = list.Take(indx).ToList();
-                }
+                        indx = i-1;  
+                        break;                                     
+                    }                
             }
 
-            if(indx == 0) return -1;
+            if(indx < 0) return -1;
 
+            var res = list.Take(indx).ToList();
             var skipped = list.Skip(indx+1).ToList();
             var min = skipped.Min(s => s > list[indx]) ? skipped.Min() : list[indx+1];
             skipped.Remove(min);
             skipped.Add(list[indx]);
-            res.Add(min);
             skipped.Sort();
+            res.Add(min);            
             res.AddRange(skipped);
             return Convert.ToInt32(String.Join("", res.ToArray()));            
         }
