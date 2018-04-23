c37cea11f5969b8455936758b6dedbdb77fb3752
Zepsen <zepsendm@gmail.com>
2018-04-23 12:43:18 +0300

    cats and dogs

---
+++ CatsAndDogs.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Codewars
+{
+    public class CatsAndDogs : ICodeWars
+    {        
+        public object Start()
+        {
+            return Func(new List<char>{'D','C','C','D','C'}, 2);
+        }
+
+        public int Func(List<char> xs, int n)
+        {
+            
+            return 0;
+        }
+    }
+}
