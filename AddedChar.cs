e5f37a31eb3299f396b5e4dfdb4ad7529605f871
Zepsen <zepsendm@gmail.com>
2018-05-05 12:38:51 +0300

    Adddedchar

--- AddedChar.cs
+++ AddedChar.cs
@@ -10,9 +10,35 @@ namespace Codewars
     {        
         public object Start()
         {          
-            return Merge(
+            return Best(
                 new Dictionary<string, int>{{"A", 1}}, 
-                new Dictionary<string, int>{{"B", 2}}
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"A", 1}}, 
+                new Dictionary<string, int>{{"B", 2}},
+                new Dictionary<string, int>{{"B", 3}},
+                new Dictionary<string, int>{{"B", 4}}
                 );
         }
 
@@ -22,7 +48,6 @@ namespace Codewars
             foreach (var item in dicts)
             {
                 if(item.Count == 0) continue;
-
                 foreach (var key in item.Keys)
                 {
                     if(res.ContainsKey(key))
@@ -37,6 +62,39 @@ namespace Codewars
             }
             
             return res;
+        }
+
+         public Dictionary<TKey, TValue[]> Merge2<TKey, TValue>(params Dictionary<TKey, TValue>[] dicts)
+        {
+            var res = new Dictionary<TKey, List<TValue>>();
+            foreach (var item in dicts)
+            {
+                if(item.Count == 0) continue;                
+                foreach (var key in item.Keys)
+                {
+                    if(res.ContainsKey(key))
+                    {
+                        res[key].Add(item[key]);
+                    }
+                    else 
+                    {
+                        res.Add(key, new List<TValue> { item[key]});
+                    }
+                }                
+            }            
+            return res.ToDictionary(i => i.Key, i => i.Value.ToArray());       
+        }
+       
+        public Dictionary<TKey, TValue[]> Best<TKey, TValue>(params Dictionary<TKey, TValue>[] dicts)
+        {
+            var s1 = dicts.SelectMany(i => i);
+            var s2 = s1.GroupBy(k => k.Key);
+            var s3 = s2.ToDictionary(g => g.Key, g => g.Select(i => i.Value).ToArray());      
+
+           return dicts
+            .SelectMany(m => m)
+            .GroupBy(p => p.Key)
+            .ToDictionary(g => g.Key, g => g.Select(i => i.Value).ToArray());      
         }
     }
 }
