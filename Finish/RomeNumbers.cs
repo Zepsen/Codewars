c719013c62b85e4ed26f6642e25bc1d5f5bd3191
Zepsen <zepsendm@gmail.com>
2018-05-06 21:20:43 +0300

    start 44

--- Finish/RomeNumbers.cs
+++ Finish/RomeNumbers.cs
@@ -6,6 +6,11 @@ namespace Codewars
 {
     public class RomeNumbers : ICodeWars
     {
+        Dictionary<char, int> dic = new Dictionary<char, int>()
+            {
+                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+                {'C', 100}, {'D', 500}, {'M', 1000},
+            };
         public object Start()
         {
             return Solution("MCMXCIV");
---
+++ Skyscrapers44.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Codewars
+{
+    public class Skyscrapers44 : ICodeWars
+    {
+        public object Start()
+        {            
+            return Func(new int [] { 
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0
+            });
+        }
+
+        private object Func(int[] arr)
+        {
+            return null;
+        }
+
+        private bool isSolved(int[][] arr)
+        {
+            return false;
+        }
+    }
+}
